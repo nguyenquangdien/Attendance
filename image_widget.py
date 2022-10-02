2b0f6a99a2259a2ab1a70ff9b63ec2c9b2b808c0
DienNguyen <dien.nguyen@skan.ai>
2022-10-02 14:52:07 +0700

    Initialize project

---
+++ image_widget.py
@@ -0,0 +1,20 @@
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtGui import QFont, QPainter, QImage, QTextCursor
+from PyQt5.QtCore import QTimer, QPoint, pyqtSignal
+
+class ImageWidget(QWidget):
+    def __init__(self, parent=None):
+        super(ImageWidget, self).__init__(parent)
+        self.image = None
+
+    def setImage(self, image):
+        self.image = image
+        self.setMinimumSize(image.size())
+        self.update()
+
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        if self.image:
+            qp.drawImage(QPoint(0, 0), self.image)
+        qp.end()
